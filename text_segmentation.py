d15a3a2ce2ff3fe4da22dd7b4542ae8ca839fb35
1250658183 <1250658183@qq.com>
2020-01-14 13:58:28 +0800

    生成双份分词数据，含/不含id

--- text_segmentation.py
+++ text_segmentation.py
@@ -15,7 +15,12 @@ def text_segment():
     targetf.write('id\tsegmented_text\n')
     targetf.close()
 
+    targetf1 = open('no_id_seg_text.tsv', 'w+', encoding="utf-8")
+    targetf1.write('segmented_text\n')
+    targetf1.close()
+
     targetf = open('segmented_text.tsv', 'a+', encoding="utf-8")
+    targetf1 = open('no_id_seg_text.tsv', 'a+', encoding="utf-8")
     for id,text in tqdm(zip(source['id'],source['sentence'])):
         ans = ""
         seg_list = jieba.cut(text)
@@ -23,7 +28,9 @@ def text_segment():
             if seg not in stop:
                 ans += ' '+seg
         targetf.write('{}\t{}\n'.format(id,ans.strip(' ')))
+        targetf1.write('{}\n'.format(ans.strip(' ')))
     targetf.close()
+    targetf1.close()
 
 if __name__ == '__main__':
     text_segment()

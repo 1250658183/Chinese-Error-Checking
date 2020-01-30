88af6f8f9a4f81da6eab228d2b45b261ef7d1c2a
1250658183 <1250658183@qq.com>
2020-01-30 21:10:50 +0800

    利用kenlm模型测试语句准确性

--- kenlm_test.py
+++ kenlm_test.py
@@ -1,4 +1,4 @@
 #encoding:utf-8
 import kenlm
-model = kenlm.Model('/home/cyc/下载/MyModel/log.bin')
-print(model.score('基层', bos=True, eos=True))
+model = kenlm.Model('/home/cyc/下载/MyModel/log_v2.bin')
+print(model.score('医药 领域 快速 发展', bos=True, eos=True))
